2ef33eba7c4b47966822f7be1ab739573923ed40
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-22 10:43:39 +0000

    优化：svlog error 方法自动打印异常堆栈；run.sh git fetch 强制匿名拉取 + 失败自动 fallback 到 GitHub

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-07-21 14:49:48"
-ts = 1784645388
+ver = "2026-07-22 10:43:39"
+ts = 1784717019
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
