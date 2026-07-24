2e53330d8fdc3fcf88900fd772dd8e9bb658c2ad
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-24 13:13:41 +0000

    image_bed 支持 pixhost 自定义域名配置

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-07-24 10:33:55"
-ts = 1784889235
+ver = "2026-07-24 13:13:41"
+ts = 1784898821
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
